69a10117003081686ce19dd20ad6e16d0b73e9c1
Eliud-Garcia <jhonniereliudgarcicapera@gmail.com>
2023-05-15 19:41:49 -0500

    arbol binario

--- src/Arboles/arbolBinario/Nodo.java
+++ src/Arboles/arbolBinario/Nodo.java
@@ -1,12 +1,5 @@
-package Arboles.arbolBinario;/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package Arboles.arbolBinario;
 
-/**
- * @author j.garcia
- */
 public class Nodo {
     private Estudiante estudiante;
     private Nodo izq, der;
