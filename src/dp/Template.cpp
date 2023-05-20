734827b9ee725345c8f3a99c6bfb5c1a6a8291da
Eliud-Garcia <jhonniereliudgarcicapera@gmail.com>
2023-05-20 12:18:57 -0500

    cpp

---
+++ src/dp/Template.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <bits/stdc++.h>
+#define put pair<char, int>
+#include <map>
+using namespace std;
+
+
+map<char, int> mapa;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+
+    return 0;
+}
